c77ab2c1f82cc6f310773a980525a1d860d95a6e
Liu Xia <lxconan@gmail.com>
2020-08-10 18:20:51 +0800

    [liuxia] refactor: remove unnecessary part.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -47,15 +47,6 @@
   }
 
   const chat = {
-    messageToSend: '',
-    messageResponses: [
-      'Why did the web developer leave the restaurant? Because of the table layout.',
-      'How do you comfort a JavaScript bug? You console it.',
-      'An SQL query enters a bar, approaches two tables and asks: "May I join you?"',
-      'What is the most used language in programming? Profanity.',
-      'What is the object-oriented way to become wealthy? Inheritance.',
-      'An SEO expert walks into a bar, bars, pub, tavern, public house, Irish pub, drinks, beer, alcohol'
-    ],
     init: function() {
       this.cacheDOM();
       this.bindEvents();
