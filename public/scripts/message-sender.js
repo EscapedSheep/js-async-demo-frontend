06ec0edde1f086251f6eea6c92ed5e968a29a471
Liu Xia <lxconan@gmail.com>
2020-08-10 18:37:27 +0800

    [liuxia] feat: sending message using XMLHttpRequest object.

--- public/scripts/message-sender.js
+++ public/scripts/message-sender.js
@@ -7,7 +7,7 @@
  * @param {(XMLHttpRequest) => void} failure The failure callback.
  */
 function postRequestSync (uri, payload, success, failure) {
-  success({ responseText: JSON.stringify(payload) });
+  post(uri, payload, success, failure, false);
 }
 
 /**
@@ -19,5 +19,28 @@ function postRequestSync (uri, payload, success, failure) {
  * @param {(XMLHttpRequest) => void} failure The failure callback.
  */
 function postRequestAsync (uri, payload, success, failure) {
-  success({ responseText: JSON.stringify(payload) });
+  post(uri, payload, success, failure, true);
+}
+
+function post(uri, payload, success, failure, isAsync) {
+  // 1 create request
+  // 1.1 create XMLHttpRequest object
+  const xhr = new XMLHttpRequest();
+  // 1.2 set url, http method and sync/async configuration
+  xhr.open('POST', uri, isAsync);
+  // 1.3 set request header
+  xhr.setRequestHeader('content-type', 'application/json');
+
+  // 2. register success & failure callbacks to request object.
+  // 2.1 register on `onload` callback.
+  xhr.onload = function () {
+    if (xhr.status >= 200 && xhr.status <= 299) {
+      success({ responseText: xhr.responseText, statusText: xhr.statusText });
+    } else {
+      failure({ responseText: xhr.responseText, statusText: xhr.statusText });
+    }
+  }
+
+  // 3 send request with payload
+  xhr.send(JSON.stringify(payload));
 }
